44766fbb1218eecdc1ab40f98c5e7351dc010a4a
donghan <handong2@student.ubc.ca>
2024-07-18 15:46:19 -0700

    port

--- backend/server/server.mjs
+++ backend/server/server.mjs
@@ -5,7 +5,7 @@ import { connectDB } from './db.mjs';
 import { getMembers, addMember, deleteMember, updateMember } from '../models/member.mjs';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
